6a29fcaeb64f8819f591956b3700d1d457849046
Ugo Nelson <theartist@live.co.uk>
2021-11-11 21:19:04 +0000

    rails watch-list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/new'
+  # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create index]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'pry'
+require 'uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+ url = 'http://tmdb.lewagon.com/movie/top_rated?page=2'
+ json = URI.open(url).read
+ data = JSON.parse(json)
+ data['results'].each do |result|
+  movie = Movie.new
+  movie.poster_url = "https://image.tmdb.org/t/p/w500#{result['poster_path']}"
+  movie.title = result['original_title']
+  movie.overview = result['overview']
+  movie.rating = result['vote_average']
+  movie.save
+ end
